373ed85bc67db17dfe44188e0e1afb55eea39c9d
Valentine <valtikaless@gmail.com>
2021-03-21 21:19:41 +0300

    data.\forEach \ images \event loop

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-let a = 9
+console.log(0)
 
-$.ajax('https://repetitora.net/api/JS/Images')
+$.ajax('https://repetitora.net/api/JS/Images', {
+    success: function (data) {
+        data.forEach(el => {
+            const img = document.createElement('img')
+            img.src = el.thumbnail
+            document.querySelector('body').appendChild(img)
+        })
+    }
+})
 
-
-console.log(a)
+console.log(1)
