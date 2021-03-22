98b58fa09c0fb3be9196b559f65f753ad0a75645
Valentine <valtikaless@gmail.com>
2021-03-22 10:35:38 +0300

    single responsibility \ callback

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const resultBlock = document.querySelector('#result')
 const pageNumber = document.querySelector('#page-number')
 const clickMeButton = document.querySelector('#click-me')
 
-clickMeButton.addEventListener('click', makeRequest)
+clickMeButton.addEventListener('click', () => {
+    getImages(pageNumber.value,onDataReceived)
 
-function makeRequest() {
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-        success: function (data) {
-            data.forEach(el => {
-                const img = document.createElement('img')
-                img.src = el.thumbnail
-                document.querySelector('#result').appendChild(img)
-            })
-        }
+})
+
+function onDataReceived (data) {
+    data.forEach(el => {
+        const img = document.createElement('img')
+        img.src = el.thumbnail
+        document.querySelector('#result').appendChild(img)
     })
 }
-
 
 //types get / post /put /delete /options
 
---
+++ service.js
@@ -0,0 +1,9 @@
+function getImages(pageNumber,successCallback) {
+    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
+        success: function (data) {
+            successCallback(data)
+        }
+    })
+}
+
+//DAL  Data Access layer
